7137925cef5853393aec07d803b6dbd91e6c4916
Vitor Oliveira <vitoroliveira.guedes@outlook.com>
2026-07-10 11:50:40 -0300

    feat: add icons dependencies for social links in hero card
    fix: dependencies configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,13 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import tailwind from "@astrojs/tailwind"
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), icon()],
 
   vite: {
     plugins: [tailwindcss()]
