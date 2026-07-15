ec6d04a8737ead1d5f62abf60991df4323a44a09
Vitor Oliveira <vitoroliveira.guedes@outlook.com>
2026-07-15 15:57:30 -0300

    feat: add blog collection configuration and fetch blog posts

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { defineCollection } from 'astro:content';
+
+import { glob } from 'astro/loaders';
+
+import { z } from 'astro/zod';
+
+const blog = defineCollection({
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  schema: z.object({
+    title: z.string(),
+    topic: z.string().optional(),
+    description: z.string(),
+    date: z.coerce.date(),
+    tags: z.array(z.string()).optional,
+  }),
+});
+
+// 5. Export a single `collections` object to register your collection(s)
+export const collections = { blog };
