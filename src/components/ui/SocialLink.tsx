13493822ac7ffdb33746a85288ab88704227d5b3
Hugo <vitoroliveira.guedes@outlook.com>
2026-07-24 16:58:56 -0300

    fix: minor layout and code changes on socialLink and hero card

--- src/components/ui/SocialLink.tsx
+++ src/components/ui/SocialLink.tsx
@@ -1,46 +1,51 @@
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "./tooltip";
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from './tooltip';
 
-import { Icon } from "@iconify/react";
+import { Icon } from '@iconify/react';
 
 export interface socialItem {
-  icon: string;
-  link: string;
-  tooltipValue?: string;
+	icon: string;
+	link: string;
+	tooltipValue?: string;
 }
 
 interface socialProps {
-  social: socialItem;
-  socialName?: string;
+	social: socialItem;
+	socialName?: string;
 }
 
-const SocialLink = ({ social, socialName }: socialProps) => {
-  return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <a
-            rel="noreferrer"
-            className="flex-center p-1  gap-1 text-xs font-detail tracking-widest text-text-main/80"
-            target="_blank"
-            href={social.link}
-          >
-            <Icon className="w-5 h-5" icon={social.icon} />
-            <span className="uppercase">{socialName}</span>
-          </a>
-        </TooltipTrigger>
-        {social.tooltipValue ? (
-          <TooltipContent className="bg-border-muted-alt text-border-muted-alt">
-            <p className="text-text-main">{social.tooltipValue}</p>
-          </TooltipContent>
-        ) : null}
-      </Tooltip>
-    </TooltipProvider>
-  );
+const SocialLink = ({
+	social,
+	socialName,
+}: socialProps) => {
+	return (
+		<TooltipProvider>
+			<Tooltip>
+				<TooltipTrigger asChild>
+					<a
+						rel="noreferrer"
+						className="flex-center p-1 hover:text-brand-hover gap-1 text-xs font-detail tracking-widest text-text-main/80"
+						target="_blank"
+						href={social.link}
+					>
+						<Icon className="w-5 h-5" icon={social.icon} />
+						<span className="uppercase ">{socialName}</span>
+					</a>
+				</TooltipTrigger>
+				{social.tooltipValue ? (
+					<TooltipContent className="bg-border-muted-alt text-border-muted-alt">
+						<p className="text-text-main">
+							{social.tooltipValue}
+						</p>
+					</TooltipContent>
+				) : null}
+			</Tooltip>
+		</TooltipProvider>
+	);
 };
 
 export default SocialLink;
