dd18465fad8c3b0e1438544b0a9bebef81bfab17
Hugo <vitoroliveira.guedes@outlook.com>
2026-09-01 14:51:41 -0300

    feat: aprimorar HeroCard com layout responsivo e otimizações de estilo

--- src/lib/siteContent.ts
+++ src/lib/siteContent.ts
@@ -5,7 +5,17 @@ const socials = ["linkedin", "github", "discord"];
 export const heroCardContent: CardProps = {
   title: "Obrigado por vir! Sou desenvolvedor Full Stack",
   description:
-    "Desenvolvo aplicações web ponta a ponta. Desde o design de interfaces intuitivas, funcionais e acessíveis até a construção do back-end, automações e infraestrutura completa,  descubra mais sobre mim.",
-  techstack: ["Typescript", "React", "Next.js", "Docker"],
+    "Desenvolvo aplicações web ponta a ponta. Desde o design de interfaces intuitivas, funcionais e acessíveis até a construção do back-end, automações e infraestrutura completa, sinta-se à vontade para explorar meu portfólio e entrar em contato. ",
+  techstack: [
+    "Typescript",
+    "React",
+    "NextJS",
+    "Docker",
+    "Fastify",
+    "NodeJS",
+    "PostgreSQL",
+    "N8N",
+    "Supabase",
+  ],
   socials: socials,
 };
